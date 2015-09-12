1d9e112e039666c9f4c90b866f06d983344d8674
Thecrazyskull <anaskarbila@gmail.com>
2015-09-13 00:29:49 +0200

    initial clark device tree bringup
    Signed-off-by: Thecrazyskull <anaskarbila@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,63 @@
+TARGET_BOOTLOADER_BOARD_NAME := MSM8992
+TARGET_NO_BOOTLOADER := true
+BOARD_CUSTOM_BOOTIMG_MK := device/motorola/clark/mkbootimg.mk
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8992
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno418
+
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_ABI := arm64-v8a
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+TARGET_USES_64_BIT_BINDER := true
+
+SMALLER_FONT_FOOTPRINT := true
+MINIMAL_FONT_FOOTPRINT := true
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/motorola/clark/kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 earlyprintk=msm_hsl_uart,0xf991e000 utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup --dt device/motorola/clark/dtb
+BOARD_KERNEL_BASE := 00000000
+BOARD_PAGE_SIZE := 4096
+BOARD_KERNEL_OFFSET := 00008000
+BOARD_RAMDISK_OFFSET := 01000000
+BOARD_TAGS_OFFSET := 00000100
+BOARD_DT_SIZE := 4280320
+
+# Bluetooth
+#BOARD_HAVE_BLUETOOTH := true
+#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/generic/common/bluetooth
+
+BOARD_USES_GENERIC_AUDIO := true
+
+#USE_CAMERA_STUB := true
+
+BUILD_EMULATOR_OPENGL := true
+USE_OPENGL_RENDERER := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 512
+
+# Recovery
+PRODUCT_COPY_FILES := device/motorola/clark/twrp.fstab:recovery/root/etc/twrp.fstab
+DEVICE_RESOLUTION := 1440x2560
+TW_IGNORE_MAJOR_AXIS_0 := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INCLUDE_CRYPTO := false
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
