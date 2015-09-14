5c518c05a812dfbc090cff2cb7ec8b8ce4ad91b6
Anas Karbila <anaskarbila@gmail.com>
2015-09-14 16:45:13 +0200

    Update BoardConfig
    *remove useless flags
    *steal some flags from hima
    *re-organize BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,12 @@
-TARGET_BOOTLOADER_BOARD_NAME := MSM8992
-TARGET_NO_BOOTLOADER := true
-BOARD_CUSTOM_BOOTIMG_MK := device/motorola/clark/mkbootimg.mk
+LOCAL_PATH := device/motorola/clark
+
+TARGET_SPECIFIC_HEADER_PATH := device/motorola/clark/include
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8992
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno418
 
+# CPU
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_VARIANT := generic
@@ -16,20 +17,41 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_USES_64_BIT_BINDER := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+
 TARGET_NO_BOOTLOADER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8992
+TARGET_NO_BOOTLOADER := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/clark/bluetooth
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+
+# Camera
+#USE_CAMERA_STUB := true
+
+# Display
+USE_OPENGL_RENDERER := true
+MALLOC_IMPL := dlmalloc
+HAVE_ADRENO_SOURCE:= false
+OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
 
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-TARGET_USES_64_BIT_BINDER := true
-
-SMALLER_FONT_FOOTPRINT := true
-MINIMAL_FONT_FOOTPRINT := true
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/motorola/clark/kernel
-TARGET_SPECIFIC_HEADER_PATH := device/motorola/clark/include
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 earlyprintk=msm_hsl_uart,0xf991e000 utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup androidboot.selinux=permisive --dt device/motorola/clark/dtb
 BOARD_KERNEL_BASE := 00000000
 BOARD_PAGE_SIZE := 4096
@@ -37,21 +59,15 @@ BOARD_KERNEL_OFFSET := 00008000
 BOARD_RAMDISK_OFFSET := 01000000
 BOARD_TAGS_OFFSET := 00000100
 BOARD_DT_SIZE := 4280320
+BOARD_CUSTOM_BOOTIMG_MK := device/motorola/clark/mkbootimg.mk
 
-# Bluetooth
-#BOARD_HAVE_BLUETOOTH := true
-#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/generic/common/bluetooth
-
-BOARD_USES_GENERIC_AUDIO := true
-
-#USE_CAMERA_STUB := true
-
-BUILD_EMULATOR_OPENGL := true
-USE_OPENGL_RENDERER := true
-
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 42024960
+# Partitions
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 42024960
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13725837312
+BOARD_CACHEIMAGE_PARTITION_SIZE := 734003200
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
@@ -68,12 +84,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
+
+# Power
 TARGET_POWERHAL_VARIANT := qcom
-
-MALLOC_IMPL := dlmalloc
-
-HAVE_ADRENO_SOURCE:= false
-
-OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-
